56344951bffb083c305baeb86213d0f7151a3064
Tyler Walker <tylerjwalker@gmail.com>
2020-05-21 14:31:36 -0600

    put transition group back in

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -13,7 +13,7 @@ import Footer from '../../components/Footer/Footer';
 import { AuthProvider } from '../../Auth/Auth';
 import PrivateRoute from '../../PrivateRoute/PrivateRoute';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 // import analytics from '../../firebase';
 
@@ -26,19 +26,19 @@ function App() {
           <Header id='header' />
 
           <Route render={({ location }) => (
-            // <TransitionGroup>
-            // <CSSTransition key={location.key} timeout={450} classNames='fade'>
-            <main>
-              <Switch location={location}>
-                <Route exact path='/' component={MainPage} />
-                <Route exact path='/about' component={About} />
-                <Route exact path='/login' component={Login} />
-                <Route exact path='/hoodPage' component={HoodPage} />
-                <PrivateRoute exact path='/addart' component={AddArt} />
-              </Switch>
-            </main>
-            //   </CSSTransition>
-            // </TransitionGroup>
+            <TransitionGroup>
+              <CSSTransition key={location.key} timeout={600} classNames='fade'>
+                <main>
+                  <Switch location={location}>
+                    <Route exact path='/' component={MainPage} />
+                    <Route exact path='/about' component={About} />
+                    <Route exact path='/login' component={Login} />
+                    <Route exact path='/hoodPage' component={HoodPage} />
+                    <PrivateRoute exact path='/addart' component={AddArt} />
+                  </Switch>
+                </main>
+              </CSSTransition>
+            </TransitionGroup>
           )} />
 
 
